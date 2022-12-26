2cb644506437623c5f0939483134c8cc8edde051
GiesserCode <121457691+GiesserCode@users.noreply.github.com>
2022-12-26 20:54:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function onstart(){
+   document.getElementById('textwelcome').innerHTML = 'Als Experten für Werbetechnik und Textilverarbeitung haben wir uns auf die Herstellung von hochwertigen Stoffen für die Werbemittelindustrie spezialisiert. Unsere Aufgabe ist es, die Wünsche unserer Kunden in die Tat umzusetzen und ihnen individuelle Stoffe für ihre Werbemittel anzubieten.';
+}
+
+function onclickwelcomedes(){
+    console.log('pressed welcome button');
+}
+
+function onclickaboutusdes(){
+    console.log('pressed about us button');
+}
+
+function onclickfoliendes(){
+    console.log('pressed folien button');
+}
+
+function onclicktextildes(){
+    console.log('pressed textil button');
+}
+
+function onclickcontactdes(){
+    console.log('pressed contact button');
+}
